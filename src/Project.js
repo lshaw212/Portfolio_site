cdaf017b4e3c554fa465ccc04ceaaa8b23fd75a8
lshaw212 <lshaw212@gmail.com>
2019-04-06 21:37:53 +0100

    Changes to Project list & how things are displayed

--- src/Project.js
+++ src/Project.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 const Project = ({name, desc, code, website}) => (
   <div className="project-container">
-    <div className="project-image"></div>
-    <div className="project-content">
-      <div className="project-name">{name}</div>
-      <div className="project-desc">{desc}</div>
-      <a href={code}>code</a>
-      <a href={website}>website</a>
-    </div>
+    <Fade left>
+      <div className="project-image">
+        <img src="https://i.imgur.com/4Jm5K2s.jpg" alt=""/>
+      </div>
+    </Fade>
+    <Fade right>
+      <div className="project-content">
+        <div className="project-name">{name}</div>
+        <div className="project-desc">{desc}</div>
+
+        <div className="project-links">
+          <a href={code}>code</a>
+          <a href={website}>website</a>
+        </div>
+      </div>
+    </Fade>
   </div>
 )
 
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -10,38 +10,31 @@ const ProjectList =() => (
       title="Projects"
       desc="This is a test"
     />
-    {/* duration={500} */}
-    <Fade left >
-      <Project
-        name="Blog"
-        desc="Blog@ is a website to create and share blogs with multiple 
-              users in a simple and easy way. This project was fully built 
-              with React on the frontend and Node.js with the Express.js 
-              framework on the backend, while also utilizing MongoDB for 
-              storing data. With a RESTful API I was able to showcase many 
-              of my skills in creating a full stack application."
-        code="https://www.github.com"
-        website="www.google.com"
-      />
-    </Fade>
-    <Fade right>
-      <Project
-        name="React Calc"
-        desc="This small calculator project was built with React, providing 
-              the 4 basic operators, numpad functionality, tweaked for mobile 
-              and styled to replicate an old casio calculator."
-        code="https://www.github.com"
-        website="https://www.google.com"
-      />
-    </Fade>
-    <Fade left>
-      <Project
-        name="Portfolio Site"
-        desc="To add..."
-        code="https://www.github.com"
-        website="https://www.google.com"
-      />
-    </Fade>
+    <Project
+      name="Blog@"
+      desc="Blog@ is a website to create and share blogs with multiple 
+            users in a simple and easy way. This project was fully built 
+            with React on the frontend and Node.js with the Express.js 
+            framework on the backend, while also utilizing MongoDB for 
+            storing data. With a RESTful API I was able to showcase many 
+            of my skills in creating a full stack application."
+      code="https://github.com/lshaw212/blogat"
+      website="https://blogat-ls.herokuapp.com/"
+    />
+    <Project
+      name="React Calc"
+      desc="This small calculator project was built with React, providing 
+            the 4 basic operators, numpad functionality, tweaked for mobile 
+            and styled to replicate an old casio calculator."
+      code="https://github.com/lshaw212/react_calc"
+      website="https://lshaw-react-calc.herokuapp.com/"
+    />
+    <Project
+      name="Portfolio Site"
+      desc="To add..."
+      code="https://github.com/lshaw212/Portfolio_site"
+      website="https://www.google.com"
+    />
   </div>
 )
 
