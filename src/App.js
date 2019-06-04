7cd7a4149d33a940d5f2a31976ab2202da474bf4
lshaw212 <lshaw212@gmail.com>
2019-06-04 19:05:31 +0100

    Navbar redesign

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ class App extends Component {
   scrollFunction(){
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("navbar").style.backgroundColor = "grey";
-      document.getElementById("navbar").style.padding = "20px 10px";
+      document.getElementById("navbar").style.padding = "10px 10px";
       // document.getElementById("logo").style.fontSize = "25px";
     } else {
       document.getElementById("navbar").style.backgroundColor = "transparent";
-      document.getElementById("navbar").style.padding = "60px 10px";
+      document.getElementById("navbar").style.padding = "30px 10px";
       // document.getElementById("logo").style.fontSize = "35px";
     }
   }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Navbar = ({scroll,drawerToggleClickHandler}) => (
   <div id="navbar">
-    <button onClick={() => {document.querySelector('#homepage-container').scrollIntoView({ behavior: 'smooth' })}} id="logo">
-      LEWIS <strong id="navbar-dot">·</strong> SHAW
+    <button onClick={() => {document.querySelector('#homepage-container').scrollIntoView({ behavior: 'smooth' })}} id="navbar-logo">
+      <div>L</div><div id="navbar-dot">·</div><div>S</div>
     </button>
     <div id="navbar-right">
-      <button onClick={() => {document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' })}}>Projects</button>
-      <button onClick={() => {document.querySelector('#skills').scrollIntoView({ behavior: 'smooth' })}}>Skills</button>
-      <button onClick={() => {document.querySelector('#about').scrollIntoView({ behavior: 'smooth' })}}>About</button>
-      <button onClick={() => {document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' })}}>Contact</button>
+      <button className="navbar-btn" onClick={() => {document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' })}}>Projects</button>
+      <button className="navbar-btn" onClick={() => {document.querySelector('#skills').scrollIntoView({ behavior: 'smooth' })}}>Skills</button>
+      <button className="navbar-btn" onClick={() => {document.querySelector('#about').scrollIntoView({ behavior: 'smooth' })}}>About</button>
+      <button className="navbar-btn" onClick={() => {document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' })}}>Contact</button>
     </div>
     <div id="navbar-toggle">
       <button onClick={drawerToggleClickHandler}>///</button>
