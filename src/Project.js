6b4ea878a7c89e87201e2dd035e333bcf71ebb62
lshaw212 <lshaw212@gmail.com>
2019-06-27 16:09:13 +0100

    ProjectList changes

--- src/Project.js
+++ src/Project.js
@@ -3,29 +3,18 @@ import Fade from 'react-reveal/Fade';
 
 const Project = ({name, image, image2, desc, desc_extra, code, website}) => (
   <div className="project-container">
-    {/* <Fade left> */}
-        {image2 ?(
-          <div className="project-image">
-            <img src={image} className="img1" alt="project image" />
-            <img src={image2} className="img2" alt="project second image" />
-          </div>
-        ) : (
-          <div className="project-image">
-            <img src={image} alt="project image" />
-          </div>
-        )}
-    {/* </Fade> */}
-    {/* <Fade right> */}
-      <div className="project-content">
-        <div className="project-name">{name}</div>
-        <div className="project-desc light-text">{desc}</div>
-        <div className="project-desc light-text">{desc_extra}</div>
-        <div className="project-links">
-          <a className="navbar-btn" href={code}>Github</a>
-          <a className="navbar-btn" href={website}>Demo</a>
-        </div>
+    <div className="project-image">
+      <img src={image} alt="project image" />
+    </div>
+    <div className="project-content">
+      <div className="project-name">{name}</div>
+      <div className="project-desc light-text">{desc}</div>
+      <div className="project-desc light-text">{desc_extra}</div>
+      <div className="project-links">
+        <a className="navbar-btn" target="_blank" href={code}>Github</a>
+        <a className="navbar-btn" target="_blank" href={website}>Demo</a>
       </div>
-    {/* </Fade> */}
+    </div>
   </div>
 )
 
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Project from './Project';
 import Heading from './Heading';
-import blogImage1 from './images/Blog@.jpg';
-import calcImage1 from './images/ReactCalc.jpg';
-import blogImage2 from './images/Blog@2.jpg'
-import calcImage2 from './images/ReactCalc2.jpg';
-import Fade from 'react-reveal/Fade';
+import blogImage from './images/Blog@.jpg';
+import calcImage from './images/ReactCalc.jpg';
+import portfolioImage from './images/portfolio.jpg';
 
 const ProjectList =() => (
   <div id="project-list-container">
@@ -17,8 +15,7 @@ const ProjectList =() => (
     />
     <Project
       name="Blog@"
-      image={blogImage1}
-      // image2={blogImage2}
+      image={blogImage}
       desc="Blog@ is a web application built to create and share blogs with multiple users. This project was built using React with Node.js and the Express.js framework for the backend. Blog@ uses a RESTful API to fetch data from  MongoDB and also utilizes Redux to manage application and user state."
       desc_extra="As my first full stack application, I feel I was able to showcase many different skills to bring together a functioning application."
       code="https://github.com/lshaw212/blogat"
@@ -27,7 +24,7 @@ const ProjectList =() => (
     <hr className="blue-hr"/>
     <Project
       name="React Calculator"
-      image={calcImage1}
+      image={calcImage}
       desc="This small project was created to showcase a calculator being built as a web app. Programmed with the React library, it utilizes reusable components while maintaining the use of the 4 basic operators and a numpad functionality. Designed around web and mobile, this calculator is styled to replicate an old casio calculator."
       code="https://github.com/lshaw212/react_calc"
       website="https://lshaw-react-calc.herokuapp.com/"
@@ -35,11 +32,11 @@ const ProjectList =() => (
     <hr className="blue-hr"/>
     <Project
       name="Portfolio Site"
-      image="https://i.imgur.com/4Jm5K2s.jpg"
+      image={portfolioImage}
       desc="My personal portfolio was built around the use of the React library, and is designed to be a fully functioning portfolio for both web and mobile. Showcasing my skills and projects to future employers while also serving as a hub for all my contact details and updates."
       desc_extra="Future plans to this portfolio would be to include a personal blog."
       code="https://github.com/lshaw212/Portfolio_site"
-      website="https://www.google.com"
+      website="https://lewisshaw.dev"
     />
   </div>
 )
